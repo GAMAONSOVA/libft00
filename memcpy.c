ff0c0697837c86c0b3ef53d7d0eb88cc7eecc7e8
GAMAONSOVA <155147709+GAMAONSOVA@users.noreply.github.com>
2024-07-02 12:06:14 +0300

    Create memcpy.c

---
+++ memcpy.c
@@ -0,0 +1,13 @@
+char
+ft_memcpy(char *str, char *mtr, int a)
+{
+        int i = 0;
+        int l = 0;
+
+        while(mtr[l] != a)
+        {
+                mtr[l] = str[i];
+                i++;
+                l++;
+        }
+}
