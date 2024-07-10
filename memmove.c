e19d25fe8d409e3da252c5d78a5e1db9b3cc04f9
GAMAONSOVA <155147709+GAMAONSOVA@users.noreply.github.com>
2024-07-10 13:02:13 +0300

    Update memmove.c

--- memmove.c
+++ memmove.c
@@ -1,23 +1,25 @@
 #include <string.h>
+#include <stdio.h>
 #include <stddef.h>
 
 void*
 ft_memmove( void *dest,  const void *src, size_t a)
 {
         size_t i = 0;
-        unsigned char const *s = src;
-        unsigned char *d = dest;
+        const char *s = (char *)src;
+        char *d = (char *)dest;
 
-        if(*s < *d)
+        if(d < s)
         {
-                memcpy( d, s, a );
+                 while (a-- > 0)
+                        d[a] = s[a];
         }
         else
         {
                 while(i != a)
                 {
                         d[i] = s[i];
-                        i--;
+                        i++;
                 }
         }
         return dest;
